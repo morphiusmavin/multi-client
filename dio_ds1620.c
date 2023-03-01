59f36d89a2610cd5e4ebbccbc870ffa8d48e8f13
Dan Hampleman <raweggeater1256@gmail.com>
2023-02-28 19:54:55 -0700

    added dio_ds1620

---
+++ dio_ds1620.c
@@ -0,0 +1,181 @@
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<time.h>
+#include <sys/time.h>
+#include "nbus.h"
+#include "dio_ds1620.h"
+
+static int current_ds;
+/*********************************************************************************/
+static void mydelay(unsigned long i)
+{
+	unsigned long j;
+	struct timespec sleepTime;
+	struct timespec rettime;
+	sleepTime.tv_sec = 0;
+	sleepTime.tv_nsec = 948540;
+	for(j = 0;j < i;j++)
+	{
+		nanosleep(&sleepTime, &rettime);
+//		printf(".");
+	}
+}
+/*********************************************************************************/
+int get_pin(int pin)		// see dio.c
+{
+	unsigned short reg;
+	if(pin >= 0 && pin <= 69) 
+	{
+		reg = nbus_peek16(0x8+(6*(pin/16)));
+		return ((reg >> (pin%16)) & 1);
+	}
+	return 0;
+}
+/*********************************************************************************/
+void set_dir(int pin, int dir)
+{
+	if(dir > 0)
+		nbus_poke16(0x28, pin | 0x80);
+	else nbus_poke16(0x28, pin & ~0x80);
+}
+/*********************************************************************************/
+void set_pin(int pin, int val)
+{
+	if(val > 0)
+		nbus_poke16(0x26, pin | 0x80);
+	else nbus_poke16(0x26, pin & ~0x80);
+}
+/*********************************************************************************/
+void initDS1620(void)
+{
+	// All pins -> output
+	int pin = DQ;
+	set_dir(pin,HIZ);
+	pin = CLK;
+	set_dir(pin,OUT);
+	pin = RST_0;
+	set_dir(pin,OUT);
+	pin = RST_1;
+	set_dir(pin,OUT);
+	pin = RST_2;
+	set_dir(pin,OUT);
+	pin = RST_3;
+	set_dir(pin,OUT);
+	pin = RST_4;
+	set_dir(pin,OUT);
+	pin = RST_5;
+	set_dir(pin,OUT);
+	current_ds = RST_0;
+
+	writeCommandTo1620( DS1620_CMD_WRITECONF, 0x02 );			// CPU mode; continous conversion
+//	writeByteTo1620( DS1620_CMD_STARTCONV );					// Start conversion
+}
+/*********************************************************************************/
+static void shiftOutByte( UCHAR val )
+{
+	int i;
+	// Send UCHAR, LSB first
+	for( i = 0; i < 8; i++ )
+	{
+		set_pin(CLK,LOW);
+		
+		// Set bit
+		if( val & (1 << i))
+		{
+			set_pin(DQ,HIGH);
+			mydelay(2);
+		}
+		else
+		{
+			set_pin(DQ,LOW);
+			mydelay(2);
+		}
+		set_pin(CLK,HIGH);
+	}
+}
+/*********************************************************************************/
+// call this about 30ms before a call to readTempFrom1620
+void writeByteTo1620( UCHAR cmd )		
+{
+	set_pin(current_ds,HIGH);
+
+	shiftOutByte( cmd );
+	
+	set_pin(current_ds,LOW);
+}
+/*********************************************************************************/
+// just used in the init function
+static void writeCommandTo1620( UCHAR cmd, UCHAR data )
+{
+	set_pin(current_ds,HIGH);
+	
+	shiftOutByte( cmd );	// send command
+	shiftOutByte( data );	// send 8 bit data
+	
+	set_pin(current_ds,LOW);
+}
+/*********************************************************************************/
+// not currently used btw
+static void writeTempTo1620( UCHAR reg, int temp )
+{
+	UCHAR lsb = temp;					// truncate to high UCHAR
+	UCHAR msb = temp >> 8;				// shift high -> low UCHAR
+	
+	set_pin(current_ds,HIGH);
+	
+	shiftOutByte( reg );	// send register select
+	shiftOutByte( lsb );	// send LSB 8 bit data
+	shiftOutByte( msb );	// send MSB 8 bit data (only bit 0 is used)
+	
+	set_pin(current_ds,LOW);
+}
+/*********************************************************************************/
+int readTempFrom1620()
+{
+	int i;
+	int state;
+	
+	set_pin(current_ds,HIGH);
+	
+	shiftOutByte( DS1620_CMD_READTEMP );						// send register select
+	
+	set_dir(DQ,IN);
+	int raw = 0;
+	
+	for( i=0; i<9; i++ )										// read 9 bits
+	{
+		set_pin(CLK,LOW);
+
+		mydelay(10);
+		state = get_pin(DQ);
+		if(state == HIGH)
+			raw |= (1 << i);									// add value
+		set_pin(CLK,HIGH);
+	}
+	
+	set_pin(current_ds,LOW);
+	
+	set_dir(DQ,OUT);
+//	return (double)(raw/(double)2);								// divide by 2 and return
+	return raw;
+}
+/*********************************************************************************/
+int main(void)
+{
+	int i;
+	int val;
+	for(i = 0;i < 20;i++)
+	{
+		val = readTempFrom1620();
+		printf("%d\n",val);
+		mydelay(1000);
+	}
+
+	return 0;
+}
---
+++ dio_ds1620.h
@@ -0,0 +1,57 @@
+typedef unsigned char UCHAR;
+static void writeCommandTo1620( UCHAR cmd, UCHAR data );
+static void writeTempTo1620( UCHAR reg, int temp );
+
+/*
+DIO pins on bottom right of card when 
+looking down at it with eth port to 
+the right
+
+gnd	x	x	15	x	x	x	x
+14	13	11	10	8	7	6	4
+
+pin 4	DQ
+pin 6	CLK
+pin 7 	RST_0
+pin 8	RST_1
+pin 10	RST_2
+pin 11	RST_3
+pin 13	RST_4
+pin 14	RST_5
+pin 15	RST_6
+
+the DQ & CLK go to all the DS1620's (upto 7)
+and each RST is just the chip enable for 
+each DS1620 chip 
+
+nbus_poke16(0x28, pin | 0x80);		set pin as output
+nbus_poke16(0x28, pin & ~0x80);		set pin as input
+
+nbus_poke16(0x26, pin & ~0x80);		turn output off
+nbus_poke16(0x26, pin | 0x80);		turn output on
+*/
+#define DQ		4
+#define CLK		6
+#define RST_0	7
+#define RST_1	8
+#define RST_2	10
+#define RST_3	11
+#define RST_4	13
+#define RST_5	14
+#define RST_6	15
+#define HIGH	1
+#define LOW		0
+#define HIZ		2
+#define OUT		HIGH
+#define IN		LOW
+#define DS1620_CMD_READTEMP		0xAA
+#define DS1620_CMD_WRITETH		0x01
+#define DS1620_CMD_WRITETL		0x02
+#define DS1620_CMD_READTH		0xA1
+#define DS1620_CMD_READTL		0xA2
+#define DS1620_CMD_READCNTR		0xA0
+#define DS1620_CMD_READSLOPE	0xA9
+#define DS1620_CMD_STARTCONV	0xEE
+#define DS1620_CMD_STOPCONV		0x22
+#define DS1620_CMD_WRITECONF	0x0C
+#define DS1620_CMD_READCONF		0xAC
